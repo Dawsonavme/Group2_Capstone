c5d8f2cae7f9cea9b01951873e05439b0d668a5f
Khushi Arora <arora91@sheridancollege.ca>
2026-07-06 14:07:43 -0400

    updated front end

---
+++ frontend/src/app/pages/my-driving/my-driving.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-driving',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-driving.html',
+  styleUrl: './my-driving.css'
+})
+export class MyDriving {}
---
+++ frontend/src/app/pages/trends/trends.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trends',
+  standalone: true,
+  imports: [],
+  templateUrl: './trends.html',
+  styleUrl: './trends.css'
+})
+export class Trends {}

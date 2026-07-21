7f940b296025c2fbf7768a62d3ab10bb183d47e0
Dawsonavme <dawsonavmelin@gmail.com>
2026-07-21 15:49:28 -0400

    Configured for testing on phsycial device

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'driver-safety-tabs',
-  webDir: 'dist/driver-safety-tabs/browser'
+  webDir: 'dist/driver-safety-tabs/browser',
+  server: {
+    androidScheme: 'http'
+  }
 };
 
 export default config;

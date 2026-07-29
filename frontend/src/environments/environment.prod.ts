48f3dd37568785372e8a0f81fa77397a1c1b95b4
Dawsonavme <dawsonavmelin@gmail.com>
2026-07-28 22:54:26 -0400

    Route API through nginx proxy for single ngrok tunnel

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.196.156.37:8080'
+  apiUrl: ''
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://gear-slapstick-erasable.ngrok-free.dev'
+  production: false,
+  apiUrl: 'http://localhost:8080'
 };
